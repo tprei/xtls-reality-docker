b7090573dba09e405b714deaa87d4cd8846c7ddc
37089330+tprei@users.noreply.github.com <diyage@mini.localdomain>
2026-02-17 03:07:21 +0800

    Use IPv6 address for client config

--- get-client-qr.sh
+++ get-client-qr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-EXT_IP="${FLY_APP_NAME}.fly.dev"
+IPV6="2a09:8280:1::d4:c272:0"
 UUID=$(cat config/uuid)
 PUB_KEY=$(cat config/public)
 
-echo "vless://${UUID}@${EXT_IP}:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${SHORT_ID}#MyVLESS" > config/client_qr.txt
+echo "vless://${UUID}@[${IPV6}]:443?security=reality&encryption=none&pbk=${PUB_KEY}&headerType=none&fp=chrome&type=tcp&flow=xtls-rprx-vision&sni=${SNI}&sid=${SHORT_ID}#MyVLESS" > config/client_qr.txt
 qrencode -t ansiutf8 < config/client_qr.txt
--- get-client-settings.sh
+++ get-client-settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Address: ${FLY_APP_NAME}.fly.dev"
+echo "Address: [2a09:8280:1::d4:c272:0]:443"
 echo "UUID: $(cat config/uuid)"
 echo "Public key: $(cat config/public)"
 echo "SNI: ${SNI}"
